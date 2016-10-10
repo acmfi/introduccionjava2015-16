5bb3ca79a57090e07a8e38a147657267bbacde13
victorvillar <victor.villar.ruiz@alumnos.upm.es>
2016-10-10 12:43:58 +0200

    añadido ejemplo.java

---
+++ ejemplo.java
@@ -0,0 +1,13 @@
+public class ejemplo{
+
+
+    public static void main(String[] args) {
+        int N = 8;
+        for (int i = 0; i < N; i++) {
+            System.out.println("Hola "+ i);
+
+        }
+
+
+    }
+}
